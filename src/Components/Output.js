c3f562d82586d628010dd1640dc3b20715ee92a4
Akshay Veera <akshayveera01@gmail.com>
2024-02-20 22:42:26 +0530

    done with this

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -14,8 +14,8 @@ const Output = () => {
     
   return (
     <div className=' h-full w-full flex flex-col items-center pt-5'>
-        <div className='md:text-3xl mb-3 font-bold text-purple-800'>Basket</div>
-        <div className=' h-[85%] w-[550px] text-5xl rounded py-4 px-7 bg-gradient-to-t from-blue-400'>
+        <div className='md:text-3xl mb-3 font-bold text-purple-800'>Fruits Basket</div>
+        <div className=' h-[87%] w-[550px] text-5xl rounded py-4 px-7 bg-gradient-to-t from-blue-400'>
             {itemsList.map((val, i)=>{
                 return <button className='m-2 mt-2' key={i} onClick={()=>{
                     removeFruit(i);
